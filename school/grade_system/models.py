ee16a5b665c4c1afca7704293b8bded0bba15a7e
Aleljx <163730575+Aleljx@users.noreply.github.com>
2025-07-27 12:41:26 +0300

    Add class Class and Student and tied them up

--- school/grade_system/models.py
+++ school/grade_system/models.py
@@ -8,3 +8,11 @@ class Teacher(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+class Class(models.Model):
+    name = models.CharField(max_length=10)
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    in_class = models.ForeignKey(Class, on_delete=models.CASCADE)
