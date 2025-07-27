5192993fabcf8e336a706f9a8f6bd9ac01e1374d
Aleljx <163730575+Aleljx@users.noreply.github.com>
2025-07-27 12:26:58 +0300

    Add class Subject and Teacher and tied them up

--- school/grade_system/models.py
+++ school/grade_system/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=30)
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
